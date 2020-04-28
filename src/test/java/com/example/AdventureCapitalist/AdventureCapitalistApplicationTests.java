b9b43c74f015e6019db2c65e8c0a49f90c54f84f
Simon <simon.valat@sfr.fr>
2020-04-28 15:24:15 +0200

    Project Init

---
+++ src/test/java/com/example/AdventureCapitalist/AdventureCapitalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdventureCapitalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureCapitalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
